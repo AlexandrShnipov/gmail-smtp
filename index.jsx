d5b0de0874b28d6de9dde721a4fb060a0f43e7a6
AlexandrShnipov <sshnipov@gmail.com>
2022-01-03 00:47:29 +0300

    tested

--- index.jsx
+++ index.jsx
@@ -55,22 +55,22 @@ app.post('/sendMessage', async (req, res) => {
     html: `<b>Сообщение с вашего портфолио</b>
     <table>
 <tr>
-   <td>name:</td><td>${name}</td> 
+   <td>name:</td>  <td>${name}</td> 
 </tr>
 <tr>
-   <td>email:</td><td>${email}</td>
+   <td>email:</td>  <td>${email}</td>
 </tr>
 <tr>
-   <td>message:</td><td>${message}</td>
+   <td>message:</td>  <td>${message}</td>
 </tr>
 <tr>
-   <td>position:</td><td>${position}</td>
+   <td>position:</td>  <td>${position}</td>
 </tr>
 <tr>
-   <td>company:</td><td>${company}</td>
+   <td>company:</td>  <td>${company}</td>
 </tr>
 <tr>
-   <td>testimonial:</td><td>${testimonial}</td>
+   <td>testimonial:</td>  <td>${testimonial}</td>
 </tr>
     </table>`
   });
