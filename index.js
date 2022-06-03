15e8b6f7ff51a77eaad1605dd95bf91f8e8d8ef1
AlexandrShnipov <sshnipov@gmail.com>
2022-06-04 00:46:36 +0300

    rename index.jsx to  index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 
 // const port = 3010;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
 app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -32,7 +38,7 @@ let transporter = nodemailer.createTransport({
 
 app.get('/', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
-  res.send('Hello World!!!')
+  res.send('Hello World!')
 });
 
 //! 3
