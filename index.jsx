a5b059cdac8da8a66bc520482a219636c3a25645
Alexandr <sshnipov@gmail.com>
2022-06-04 02:10:01 +0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -10,6 +10,11 @@ const app = express();
 
 // const port = 3010;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(cors())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
