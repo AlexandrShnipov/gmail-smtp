0e6700bd052fc30459e9dfc953fc4012a9d9c61b
Alexandr <sshnipov@gmail.com>
2022-06-04 02:13:09 +0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -10,10 +10,6 @@ const app = express();
 
 // const port = 3010;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 app.use(cors())
 // parse application/x-www-form-urlencoded
@@ -36,7 +32,6 @@ let transporter = nodemailer.createTransport({
 });
 
 app.get('/', (req, res) => {
-   res.header('Access-Control-Allow-Origin'
   res.send('Hello World!')
 });
 
