65b41bbc5d26e106645fc21157c4cdab6425dd6a
Alexandr <sshnipov@gmail.com>
2022-06-04 01:57:57 +0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://smtp-nodejs-my-server.herokuapp.com/sendMessage');
   next();
 });
 
