4a7a1f4ae77180a261937ebef9b62cf0d968952d
AlexandrShnipov <sshnipov@gmail.com>
2022-06-05 23:39:24 +0300

    refactor index.js

--- index.js
+++ index.js
@@ -30,23 +30,38 @@ let transporter = nodemailer.createTransport({
   },
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+//testing
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+//
+// app.options('/*', (_, res) => {
+//   res.sendStatus(200);
+// });
+//
+// app.get('/', (req, res, next) => {
+//   res.send('Hello World!')
+// });
+//
+// //! 3
+// app.post('/', async (req, res, next) => {
+//   // Handle the post for this route
+
+  //testing
 
 app.options('/*', (_, res) => {
   res.sendStatus(200);
 });
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 //! 3
-app.post('/', async (req, res, next) => {
-  // Handle the post for this route
+app.post('/sendMessage', async (req, res) => {
+
   let { name, email, message, position, company, testimonial } = req.body;
 
   // send mail with defined transport object
